e1dbb904e48a767fc58b5266f71a4b7549715717
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2024-12-02 10:31:33 +0100

    Sprites, fonts
    some images 4 readme,  changed formats of some files,

--- Assets/LeaveMyAlpaca/Advanced tooltips/Samples/Buidlings/Building.cs
+++ Assets/LeaveMyAlpaca/Advanced tooltips/Samples/Buidlings/Building.cs
@@ -1,10 +1,13 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Building")]
-public class Building : ScriptableObject
+namespace AdvancedTooltips
 {
-    public Sprite icon;
-    public List<AdvancedTooltips.Core.TooltipsStatic.MaterialsDisplay> production;
-    public List<AdvancedTooltips.Core.TooltipsStatic.MaterialsDisplay> constructionCosts;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "Building")]
+    public class Building : ScriptableObject
+    {
+        public Sprite icon;
+        public List<AdvancedTooltips.Core.TooltipsStatic.MaterialsDisplay> production;
+        public List<AdvancedTooltips.Core.TooltipsStatic.MaterialsDisplay> constructionCosts;
+    }
 }
